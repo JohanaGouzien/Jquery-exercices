b1f45fa4f2dc51313053e3d4f38da0d9466c227c
gouzien <johana.gouzien+lamanu@gmail.com>
2020-11-06 14:19:13 +0100

    Exo 4 et 5

---
+++ Exercice 4/assets/js/main4.js
@@ -0,0 +1,19 @@
+//Vider le contenu paragraphe
+
+$(document).ready(function() {
+
+    $('#firstText').empty();
+
+});
+
+//Supprimer le contenu paragraphe 
+
+$(document).ready(function(){
+
+    $('#secondText').remove();
+    
+
+});
+
+
+
---
+++ Exercice 5/assets/js/main5.js
@@ -0,0 +1,8 @@
+//Donner aux élément 'li' l'attribut = texte rouge
+
+$(document).ready(function() {
+
+        $('ul').css("color", "red");
+      
+
+});
